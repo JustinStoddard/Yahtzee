ae7b9dee0f0171036bb6ef80ae16347ee8bf1dc3
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-03-28 11:59:45 -0600

    New Files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { 
+  ProtectedRoute,
+  Login,
+  Register,
+  NavBar,
+  FetchUser,
+} from '@devpoint/dps-react-kit';
+import {
+  login,
+  register,
+  logout,
+  validateToken,
+} from './actions/auth';
 import Game from './components/Game';
 
-const App = () => <Game />
+const App = () => {
+  <div>
+    <NavBar handleLogout={logoout}/>
+    <FetchUser validateToken={validateToken}>
+      <Switch>
+        <ProtectedRoute exact path="/" component={Game}/>
+        <Route exact path="login" render={ props =>
+          <Login {...props} handleLogin={Login}/>
+        } />
+        <Route exact path="/register" render={props =>
+          <Register {...props} registerUser={registerUser}/>
+        } />
+      </Switch>
+    </FetchUser>
+  </div>
+}
 
 export default App
--- client/src/reducers/currentGame.js
+++ client/src/reducers/currentGame.js
@@ -1,20 +1,24 @@
-import { ROLL_DICE, TOGGLE_KEPT } from '../actions/currentGame';
+import { 
+  ROLL_DICE, 
+  TOGGLE_KEPT,
+  UPDATE_SCORE,
+  RESET_ROLL,
+} from '../actions/currentGame';
 
 const scores = [
-  { section: 'upper', name: 'Ones', score: null, value: 1},
-  { section: 'upper', name: 'Twos', score: null, value: 2},
-  { section: 'upper', name: 'Threes', score: null, value: 3},
-  { section: 'upper', name: 'Fours', score: null, value: 4},
-  { section: 'upper', name: 'Fives', score: null, value: 5},
-  { section: 'upper', name: 'Sixes', score: null, value: 6},
-
-  { section: 'upper', name: 'Three Of A Kind', score: null, addAll: true},
-  { section: 'upper', name: 'Four Of A Kind', score: null, addAll: true},
-  { section: 'upper', name: 'Chance', score: null, addAll: true},
-  { section: 'upper', name: 'Full House', score: null},
-  { section: 'upper', name: 'Low Straight', score: null},
-  { section: 'upper', name: 'High Straight', score: null},
-  { section: 'upper', name: 'Yahtzee', score: null},
+  { section: 'upper', name: 'Ones', score: null, value: 1 },
+  { section: 'upper', name: 'Twos', score: null, value: 2 },
+  { section: 'upper', name: 'Threes', score: null, value: 3 },
+  { section: 'upper', name: 'Fours', score: null, value: 4 },
+  { section: 'upper', name: 'Fives', score: null, value: 5 },
+  { section: 'upper', name: 'Sixes', score: null, value: 6 },
+  { section: 'lower', name: 'Three Of A Kind', score: null, addAll: true },
+  { section: 'lower', name: 'Four Of A Kind', score: null, addAll: true },
+  { section: 'lower', name: 'Full House', score: null },
+  { section: 'lower', name: 'Low Straight', score: null }, 
+  { section: 'lower', name: 'High Straight', score: null },
+  { section: 'lower', name: 'Yahtzee', score: null },
+  { section: 'lower', name: 'Chance', score: null, addAll: true },
 ]
 
 const currentGame = (
@@ -23,26 +27,35 @@ const currentGame = (
     dice: [...new Array(5)],
     keep: [],
     scores,
-  }, 
-  action 
+  },
+  action
 ) => {
-    switch (action.type) {
-
-      case ROLL_DICE:
-        return {
-          ...state,
-          dice: action.dice,  // This is telling what redux state will look like in its next form.
-          roll: state.roll + 1 // This increments the state of roll everytime you push "ROLL DICE on the page".
-        }
-
-      case TOGGLE_KEPT:
-        return {
-          ...state,
-          keep: action.keep  //
-        }
-
-      default:
-        return state
+  switch (action.type) {
+    case RESET_ROLL:
+      return {
+        ...state,
+        roll: 0,
+        dice: [...new Array(5)],
+        keep: [] 
+      }
+    case UPDATE_SCORE:
+      return {
+        ...state,
+        scores: action.scores
+      }
+    case ROLL_DICE:
+      return {
+        ...state,
+        dice: action.dice,
+        roll: state.roll + 1
+      }
+    case TOGGLE_KEPT:
+      return {
+        ...state,
+        keep: action.keep
+      }
+    default:
+      return state
   }
 }
 
